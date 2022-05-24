e4e3775cf19aed58fdad50ceced43cba1fbde7aa
Suar <fn7sephiroth@gmail.com>
2022-05-24 19:46:30 +0200

    Versión Final Mejorada y Revisada

--- src/Controller/BDDApp.java
+++ src/Controller/BDDApp.java
@@ -5,11 +5,14 @@ import View.*;
 public class BDDApp {
 
     public static void main(String[] args) throws Exception {
+        //CREAMOS LAS VISTAS
 
         MainWindow mainSPL = new MainWindow();
         SubwindowConnection subSPL = new SubwindowConnection();
 
         subSPL.disableonstart();
+        
+        //CREAMOS EL CONTROLADOR DE AMBAS VISTAS
         SubwindowConecctionControl subcontSPL = new SubwindowConecctionControl(subSPL, mainSPL);
 
     }
